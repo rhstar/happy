5c7e3937a8884efc6c9b07c9127b7637b8ac1b29
rhstar <129238687+rhstar@users.noreply.github.com>
2026-07-04 21:52:13 +0900

    understanding dart.report inputs

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ api_key = os.getenv("DART_API_KEY")
 
 dart = OpenDartReader(api_key)
 
-# 최대주주 변동 이력 조회 (예: 삼성전자, 2023년)
-df = dart.report('005930', '최대주주변동', 2023)
+'''Get-Content "C:\Users\andyr\Documents\GitHub\happy\.venv\Lib\site-packages\opendartreader\dart_event.py"
+를 실행해서 dart.report
+'''
+
+# 최대주주 변동 이력 조회 (예: 삼성전자=005930, 2023년)
+df = dart.report('005930',bsns_year=2023 )
+# df = dart.report('005930', '최대주주변동', 2023)
 print(df)
 
 # python main.py
